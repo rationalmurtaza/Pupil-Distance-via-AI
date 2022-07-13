0c43d75aee5418e5fe3f70710a00fdb0834309e2
Murtaza <murtazal@mindfiresolutions.com>
2022-07-13 17:33:23 +0530

    added image upload function proper css and rounded distance

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,6 +9,50 @@ let ctx = ''
 let loader = document.querySelector(".loader");
 let calculate = document.querySelector("#calculate");
 let pupil_distance_text = document.querySelector(".pupil-distance-text");
+let ctxImage = document.getElementById('canvasForImage').getContext('2d');
+let mainDivForVideo = document.querySelector("#main-div-for-video");
+let mainDivForImage = document.querySelector("#main-div-for-Image");
+let cameraOnButton = document.querySelector("#camera-on-button");
+let chooseImageButton = document.querySelector("#choose-image-button");
+let imageForEyePupils = document.querySelector("#image-for-eye-pupils");
+let loaderImage = document.querySelector(".loader-image");
+
+let canvasForImage = document.querySelector("#canvasForImage");
+let calculateImagePd = document.querySelector("#calculate-image-pd");
+
+cameraOnButton.addEventListener("click", () => {
+    resetPhotoFunction();
+    mainDivForImage.style.display = "none";
+    mainDivForVideo.style.display = "";
+    startCamera();
+});
+
+chooseImageButton.addEventListener("click", () => {
+    ctxImage.clearRect(0, 0, canvasForImage.width, canvasForImage.height);
+    imageForEyePupils.value = '';
+    mainDivForVideo.style.display = "none";
+    mainDivForImage.style.display = "";
+    calculateImagePd.style.display = "none";
+    pupil_distance_text.innerHTML = "";
+})
+
+imageForEyePupils.addEventListener('change', (e) => {
+    let image = e.target.files[0];
+    ctxImage.clearRect(0, 0, canvasForImage.width, canvasForImage.height);
+    var img = new Image;
+    img.src = URL.createObjectURL(image);
+    img.onload = function() {
+        let imgWidth = this.width;
+        let imgHeight = this.height;
+        while(imgWidth > canvasForImage.width && imgHeight > canvasForImage.height) {
+            imgWidth = imgWidth / 2;
+            imgHeight = imgHeight / 2;
+        }
+        ctxImage.drawImage(img, 0, 0, imgWidth, imgHeight);
+    }
+    calculateImagePd.style.display = "";
+})
+
 video.addEventListener("playing", function () {
     setTimeout(function () {
         console.log("Stream dimensions" + video.videoWidth + "x" + video.videoHeight);
@@ -25,6 +69,11 @@ async function startCamera() {
     video.srcObject = stream;
 }
 
+calculateImagePd.addEventListener("click", () => {
+    loaderImage.style.display = "flex";
+    autoDraw(ctxImage, canvasForImage);
+})
+
 click_button.addEventListener('click', function () {
     video_image_div.style.display = "none";
     canvas.style.display = "block";
@@ -40,28 +89,33 @@ click_button.addEventListener('click', function () {
 });
 
 reset_photo.addEventListener('click', function () {
+    resetPhotoFunction();
+})
+
+let resetPhotoFunction = () => {
     canvas.style.display = "none";
     video_image_div.style.display = "";
     calculate.style.display = "";
     reset_and_calculate_buttons_div.style.display = "none";
     click_button.style.display = "block";
     pupil_distance_text.innerHTML = "";
-});
+}
 
 calculate_distance.addEventListener('click', function () {
     loader.style.display = "flex";
     calculate.style.display = "none";
-    autoDraw();
+    autoDraw(ctx, canvas);
 });
 
-startCamera()
 // AUTO CALC PD METHODS START-----------------------------------------------
 
-async function autoDraw() {
+async function autoDraw(canvas, workingCanvas) {
 
     model = await loadFaceLandmarkDetectionModel();
     //Render Face Mesh Prediction
-    renderPrediction();
+    console.log(model);
+    renderPrediction(canvas, workingCanvas);
+
 }
 
 async function loadFaceLandmarkDetectionModel() {
@@ -72,15 +126,15 @@ async function loadFaceLandmarkDetectionModel() {
     );
 }
 
-async function renderPrediction() {
+async function renderPrediction(ctx, workingCanvas) {
     const predictions = await model.estimateFaces({
-        input: ctx.getImageData(0, 0, canvas.width, canvas.height),
+        input: ctx.getImageData(0, 0, workingCanvas.width, workingCanvas.height),
     });
-
-    displayIrisPosition(predictions);
+    console.log(predictions);
+    displayIrisPosition(predictions, ctx,);
 }
 
-function displayIrisPosition(predictions) {
+function displayIrisPosition(predictions, ctx) {
     ctx.strokeStyle = "red";
     if (predictions.length > 0) {
         predictions.forEach((prediction) => {
@@ -151,13 +205,16 @@ function displayIrisPosition(predictions) {
                 let RightEyePD = (11.7 / irisWidth) * rightEyePdInDistance;
                 let pd = (11.7 / irisWidth) * pupilDistance;
                 loader.style.display = "none";
-                pupil_distance_text.innerHTML = "<h2>Your Pupil Distance is approximately " + pd.toFixed(2) + "mm</h2>"
-                                                + "<h3>Your Left Eye Monocular PD is approximately " + LeftEyePD.toFixed(2) + "mm</h3>"
-                                                + "<h3>Your Right Eye Monocular PD is approximately " + RightEyePD.toFixed(2) + "mm</h3>";
+                loaderImage.style.display = "none";
+                pupil_distance_text.innerHTML = "<h2>Your Pupil Distance is approximately " + roundToNearest5(pd*100)/100 + "mm</h2>"
+                                                + "<h3>Your Left Eye Monocular PD is approximately " + roundToNearest5(LeftEyePD*100)/100 + "mm</h3>"
+                                                + "<h3>Your Right Eye Monocular PD is approximately " + roundToNearest5(RightEyePD*100)/100 + "mm</h3>";
             }
         });
     }
 }
+
+roundToNearest5 = (num) => Math.round(num / 50) * 50;
 
 class Point {
     constructor(x, y) {
